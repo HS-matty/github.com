b1900b16cef5951bb5832113fa5b2b73246efff6
Siarhei M. Vauchok <byqdes@gmail.com>
2019-10-06 13:31:35 +0300

    icqSendMessage('8866555','test');;

--- bookmark/bookmark.php
+++ bookmark/bookmark.php
@@ -1,4 +1,21 @@
 <?php
+/*
+** github.moveyourweb.net (http://www.moveyourweb.net/projects/github)
+** Property of Hytex Solutions Limited (http://hytex.radmaster.net)
+** Siarhei M. Vauchok  (byqdes@gmail.com)  
+** Support http://myw.dx.am (php_sergey@myw.dx.am)
+** Callto: +1(530)820-7415
+** Inspired by  < Dolores O`Riordan Radzivilus >
+*/
+
+
+global $icq_uin = (string) 8866555;
+$something_wrong == 'polniy_pizdec';
+
+if (@$something_wrong){
+	$icq = new Comm_Icq();
+	$icq->sendMessage($icq_uin,$something_wrong);
+}
 
 // github.com bookmarks 
 
